f6957d840a0b71d61ec87d7172efb37c1218e084
Charles Gomes <charles.ngomes@hotmail.com>
2022-09-23 18:19:55 -0300

    projeto Menu Curso Web Moderno

---
+++ desafioMenu.js
@@ -0,0 +1,10 @@
+window.onhashchange = function (e) {
+    const oldURL = e.oldURL.split('#')[1]
+    const newURL = e.newURL.split('#')[1]
+
+
+    const oldMenu = document.querySelector(`.menu a[href='#${oldURL}']`)
+    const newMenu = document.querySelector(`.menu a[href='#${newURL}']`)
+    oldMenu && oldMenu.classList.remove('selector')
+    newMenu && newMenu.classList.add('selector')
+}
